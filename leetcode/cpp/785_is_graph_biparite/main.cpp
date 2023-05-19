1b1ea5d2c88b314b359bfa9270b5b0c5b0cf1ee0
DusanTodorovic5 <dtodorovic40@gmail.com>
2023-05-19 21:57:40 +0200

    fix: forgot to save src file

--- leetcode/cpp/785_is_graph_biparite/main.cpp
+++ leetcode/cpp/785_is_graph_biparite/main.cpp
@@ -7,32 +7,42 @@ public:
     bool isBipartite(std::vector<std::vector<int>>& graph) {
         // Initalize vector of size N with all zeros
         std::vector<int> bipartite(graph.size(), 0);
+
+        // create stack of current nodes and their flags
         std::stack<std::pair<int, int>> stack;
         std::pair<int, int> current_node;
-        
-        //for (int i = graph.size();i > 0; stack.push({--i, 1}));
 
-        stack.push({0,1});
-       
-        while (!stack.empty()) {
-            current_node = stack.top();
-            stack.pop();
-
-            if (!bipartite[current_node.first]) {
-                bipartite[current_node.first] = current_node.second;
-
-                for (int edge : graph[current_node.first]) {
-                    stack.push({edge, -current_node.second});
-                }
-                // for (int i = 0;i < graph[current_node.first].size(); stack.push({graph[current_node.first][i++], -current_node.second}));
+        // in case the graph is not fully connected, we must traverse trough other non-connected nodes
+        for (int i = 0; i < graph.size(); i++) {
+            if (bipartite[i]) {
                 continue;
             }
-            
-            if (bipartite[current_node.first] != current_node.second){
-                return false;
+
+            // push first node of graph to the stack
+            stack.push({i, 1});
+            // do iterative DFS
+            while (!stack.empty()) {
+                // pop the current node
+                current_node = stack.top();
+                stack.pop();
+
+                // if the node is not visited yet, it will be 0, which means we need to assign a flag to it
+                // if the node is visited, check if current flag is same, if not, return false
+                if (!bipartite[current_node.first]) {
+                    // assign the flag
+                    bipartite[current_node.first] = current_node.second;
+
+                    // for each of the edges, push to stack with flag reversed
+                    for (int edge : graph[current_node.first]) {
+                        stack.push({edge, -current_node.second});
+                    }
+                } else if (bipartite[current_node.first] != current_node.second){
+                    return false;
+                }
             }
         }
 
+        // the graph is biparite
         return true;
     }
 };
